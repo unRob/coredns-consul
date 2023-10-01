de64122739e64dde360c73766b4def9d8fc3fdc3
Roberto Hidalgo <un@rob.mx>
2023-09-30 21:48:33 -0600

    read addresses from static entries, if provided (#18)
    * read addresses from static entries, if provided
    
    also bump deps

--- consul_test.go
+++ consul_test.go
@@ -14,14 +14,53 @@ func TestFetchStaticServiceKey(t *testing.T) {
 	src := NewWatch(&WatchKVPath{Key: "static/path"})
 	c, _, _ := NewTestCatalog(true, src)
 
-	svc := c.ServiceFor("static-consul")
-	if svc == nil {
-		t.Fatalf("Service static-consul not found, got: %+v", c.Services())
-	}
+	t.Run("static target", func(t *testing.T) {
+		svc := c.ServiceFor("static-consul")
+		if svc == nil {
+			t.Fatalf("Service static-consul not found, got: %+v", c.Services())
+		}
 
-	if svc.Target != serviceProxyName {
-		t.Fatalf("Unexpected target: %v", svc.Target)
-	}
+		if svc.Target != serviceProxyName {
+			t.Fatalf("Unexpected target: %v", svc.Target)
+		}
+	})
+
+	t.Run("static addresses", func(t *testing.T) {
+		svc := c.ServiceFor("static-addr")
+		if svc == nil {
+			t.Fatalf("Service static-addr not found, got: %+v", c.Services())
+		}
+
+		if len(svc.Addresses) != 1 {
+			t.Fatalf("Unexpected addresses: %v", svc.Target)
+		}
+
+		if ip := svc.Addresses[0].String(); ip != "127.0.0.1" {
+			t.Fatalf("Unexpected address for static-addr: %v", ip)
+		}
+	})
+
+	t.Run("invalid static addresses", func(t *testing.T) {
+		svc := c.ServiceFor("static-addr-invalid")
+		if svc == nil {
+			t.Fatalf("Service static-addr-invalid not found, got: %+v", c.Services())
+		}
+
+		if len(svc.Addresses) != 1 {
+			t.Fatalf("Unexpected addresses for static-addr-invalid: %v", svc.Target)
+		}
+
+		if ip := svc.Addresses[0].String(); ip != "127.0.0.1" {
+			t.Fatalf("Unexpected address for static-addr-invalid: %v", ip)
+		}
+	})
+
+	t.Run("ignored static", func(t *testing.T) {
+		svc := c.ServiceFor("static-ignored")
+		if svc != nil {
+			t.Fatalf("Service static-addr found with invalid config, got: %+v", c.Services())
+		}
+	})
 }
 
 func TestFetchStaticServicePrefix(t *testing.T) {
